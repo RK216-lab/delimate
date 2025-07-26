4bca0fac2096db2db25ddcef4db7c4b05edc23bf
RK <kudorimi2010@gmail.com>
2025-07-26 17:04:25 +0900

    模倣の会

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function ReWrite(){
     }
 }
 // ページ読み込み時に初期化
-function QuaggaReset() {
+/*function QuaggaReset() {
     Quagga.init({
         inputStream: {
             type: "LiveStream",
@@ -89,4 +89,45 @@ function QuaggaReset() {
 // ボタン押したらスタート
 function QuaggaJS() {
     Quagga.start();
+}*/
+    function QuaggaReset(){
+        Quagga.init({
+            inputStream: {
+                type: "LiveStream",
+                target: document.querySelector('#container')
+            },
+            constraints: {
+                facingMode: "environment",
+            },
+            decoder: {
+                readers: [ "ean_reader" ]
+            } 
+        }, 
+        function(err) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.log("リセット完了したよ！");
+        });      
+    };
+function QuaggaJS(){
+    Quagga.start();
+        Quagga.onProcessed(function(result){
+            var ctx = Quagga.canvas.ctx.overlay;
+            var canvas = Quagga.canvas.dom.overlay;
+
+            ctx.clearRect(0, 0, parseInt(canvas.width), parseInt(canvas.height));
+
+            if (result) {
+                if (result.box) {
+                    console.log(JSON.stringify(result.box));
+                    Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, ctx, {color: 'blue', lineWidth: 2});
+                }
+            }
+        });
+
+        Quagga.onDetected(function(result){
+            document.getElementById("item-name").innerHTML = result.codeResult.code;
+        });
 }
